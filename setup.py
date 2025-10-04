80432dbc3b6d92e5e0d6ffb4e8d881209c1a59d4
Ramazan İzci <ramazanizci1638@gmail.com>
2025-10-05 01:38:07 +0300

    1.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="YokAPI",
-    version="1.2.0",
+    version="1.1.3",
     author="IZCI",
     author_email="email@example.com",
     description="Unofficial  API for YokAtlas",
